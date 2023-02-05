fd36f3f82711cbfa96513c19a35e6086bc3c77c6
Cactochan <85914469+mastercodermerwin@users.noreply.github.com>
2023-02-05 14:55:57 +0530

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,16 @@
+
+echo "installing piglatin - translator"
+
+echo "installing python3 -|" \n
+sudo apt install python3
+
+echo "installing required libs - |" \n
+pip install rich
+
+echo "adding to path - |" \n
+python3 add_path.py
+
+echo "done installing"
+
+
+echo "Now you can type in piglatin.... :)"
